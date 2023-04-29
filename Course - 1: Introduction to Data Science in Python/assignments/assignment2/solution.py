5b1e1212bd69247f37e8db52b92f3156b7b71a2b
Ilyos Nishanov <80672859+ilyos-nishanov@users.noreply.github.com>
2023-04-29 16:36:42 +0000

    Sat, Apr 29, 2023, 9:36 PM +05:00

--- Course - 1: Introduction to Data Science in Python/assignments/assignment2/solution.py
+++ Course - 1: Introduction to Data Science in Python/assignments/assignment2/solution.py
@@ -2,14 +2,16 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('assets/NISPUF17.csv')
-print(df.head())
 
-# def proportion_of_education ():
-#     df = pd.read_csv('assets/NISPUF17.csv')
-#     df = df[['EDUC1']]
-#     df = df.dropna()
-#     df = df.sort_values(by=['EDUC1'])
-#     df = df.groupby('EDUC1').size()
-#     df = df / df.sum()
-#     return df
-# print(proportion_of_education())
+#Write a function called proportion_of_education which returns the proportion of children in the
+#dataset who had a mother with the education levels equal to less than high school (<12), high school (12),
+##more than high school but not a college graduate (>12) and college degree.
+
+#This function should return a dictionary in the form of (use the correct numbers, do not round numbers):
+
+    # {"less than high school":0.2,
+    # "high school":0.4,
+    # "more than high school but not college":0.2,
+    # "college":0.2}
+
+def proportion_of_education ()
