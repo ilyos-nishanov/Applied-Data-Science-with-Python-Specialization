776e796073b837a02f6ccf6c2a00e3f41ab9008b
Ilyos Nishanov <80672859+ilyos-nishanov@users.noreply.github.com>
2023-04-29 16:25:39 +0000

    Sat, Apr 29, 2023, 9:25 PM +05:00

--- Course - 1: Introduction to Data Science in Python/assignments/assignment2/solution.py
+++ Course - 1: Introduction to Data Science in Python/assignments/assignment2/solution.py
@@ -1,3 +1,12 @@
 import pandas as pd
 import numpy as np
 
+def proportion_of_education ():
+    df = pd.read_csv('assets/NISPUF17.csv')
+    df = df[['EDUC1']]
+    df = df.dropna()
+    df = df.sort_values(by=['EDUC1'])
+    df = df.groupby('EDUC1').size()
+    df = df / df.sum()
+    return df
+print(proportion_of_education())
