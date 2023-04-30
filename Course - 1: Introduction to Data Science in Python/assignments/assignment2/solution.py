b360319102d065a70bc0e227cc37209db9f641a3
Ilyos Nishanov <80672859+ilyos-nishanov@users.noreply.github.com>
2023-04-30 11:06:14 +0000

    Sun, Apr 30, 2023, 4:06 PM +05:00

--- Course - 1: Introduction to Data Science in Python/assignments/assignment2/solution.py
+++ Course - 1: Introduction to Data Science in Python/assignments/assignment2/solution.py
@@ -3,6 +3,45 @@ import numpy as np
 from scipy import optimize
 import math
 
+# First we define the functions,
+def f (x, y) :
+    return np.exp(-(2*x*x + y*y - x*y) / 2)
+
+def g (x, y) :
+    return x*x + 3*(y+1)**2 - 1
+
+# Next their derivatives,
+def dfdx (x, y) :
+    return 1/2 * (-4*x + y) * f(x, y)
+
+def dfdy (x, y) :
+    return 1/2 * (x - 2*y) * f(x, y)
+
+def dgdx (x, y) :
+    return 2*x
+
+def dgdy (x, y) :
+    return 6*y+6
+
+from scipy import optimize
+
+def DL (xyλ) :
+    [x, y, λ] = xyλ
+    return np.array([
+            dfdx(x, y) - λ * dgdx(x, y),
+            dfdy(x, y) - λ * dgdy(x, y),
+            - g(x, y)
+        ])
+
+(x0, y0, λ0) = (-1, -1, 0)
+x, y, λ = optimize.root(DL, [x0, y0, λ0]).x
+print("x = %g" % x)
+print("y = %g" % y)
+print("λ = %g" % λ)
+print("f(x, y) = %g" % f(x, y))
+
+
+
 # First we define the functions, YOU SHOULD IMPLEMENT THESE
 def f (x, y) :
     return np.exp(x-y**2+x*y)*(-1)
