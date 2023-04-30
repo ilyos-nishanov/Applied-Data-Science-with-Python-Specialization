d5fbbd4b916aa15c9c7f0495dfb869af50c2332c
Ilyos Nishanov <80672859+ilyos-nishanov@users.noreply.github.com>
2023-04-30 11:11:55 +0000

    Sun, Apr 30, 2023, 4:11 PM +05:00

--- Course - 1: Introduction to Data Science in Python/assignments/assignment2/solution.py
+++ Course - 1: Introduction to Data Science in Python/assignments/assignment2/solution.py
@@ -1,7 +1,8 @@
-# Import libraries
+import pandas as pd
 import numpy as np
-from scipy import optimize
+import scipy
 import math
+
 
 # First we define the functions,
 def f (x, y) :
@@ -22,63 +23,3 @@ def dgdx (x, y) :
 
 def dgdy (x, y) :
     return 6*y+6
-
-from scipy import optimize
-
-def DL (xyλ) :
-    [x, y, λ] = xyλ
-    return np.array([
-            dfdx(x, y) - λ * dgdx(x, y),
-            dfdy(x, y) - λ * dgdy(x, y),
-            - g(x, y)
-        ])
-
-(x0, y0, λ0) = (-1, -1, 0)
-x, y, λ = optimize.root(DL, [x0, y0, λ0]).x
-print("x = %g" % x)
-print("y = %g" % y)
-print("λ = %g" % λ)
-print("f(x, y) = %g" % f(x, y))
-
-
-
-# First we define the functions, YOU SHOULD IMPLEMENT THESE
-def f (x, y) :
-    return np.exp(x-y**2+x*y)*(-1)
-
-def g (x, y) :
-    return math.cosh(y)+x-2
-
-# Next their derivatives, YOU SHOULD IMPLEMENT THESE
-def dfdx (x, y) :
-    return np.exp(x-y**2+x*y)*(1+y)*-1
-
-def dfdy (x, y) :
-    return np.exp(x-y**2+x*y)*(-2*y+x)*-1
-
-def dgdx (x, y) :
-    return 1
-def dgdy (x, y) :
-    return math.sinh(y)
-
-# Use the definition of DL from previously.
-def DL (xyλ) :
-    [x, y, λ] = xyλ
-    return np.array([
-            dfdx(x, y) - λ * dgdx(x, y),
-            dfdy(x, y) - λ * dgdy(x, y),
-            - g(x, y)
-        ])
-
-# To score on this question, the code above should set
-# the variables x, y, λ, to the values which solve the
-# Langrange multiplier problem.
-
-# I.e. use the optimize.root method, as you did previously.
-
-x, y, λ = optimize.root(DL, [0, 0, 1]).x
-
-print("x = %g" % x)
-print("y = %g" % y)
-print("λ = %g" % λ)
-print("f(x, y) = %g" % f(x, y))
