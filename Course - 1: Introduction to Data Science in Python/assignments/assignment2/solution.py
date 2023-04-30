92ad024d4d59aa71f89c1641503f87320f3ba189
Ilyos Nishanov <80672859+ilyos-nishanov@users.noreply.github.com>
2023-04-30 07:03:20 +0000

    Sun, Apr 30, 2023, 12:03 PM +05:00

--- Course - 1: Introduction to Data Science in Python/assignments/assignment2/solution.py
+++ Course - 1: Introduction to Data Science in Python/assignments/assignment2/solution.py
@@ -1,8 +1,28 @@
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
+from scipy import optimize
 import re
 
+# First we define the functions,
+def f (x, y) :
+    return np.exp(-(2*x*x + y*y - x*y) / 2)
+
+def g (x, y) :
+    return x*x + 3*(y+1)**2 - 1
+
+# Next their derivatives,
+def dfdx (x, y) :
+    return 1/2 * (-4*x + y) * f(x, y)
+
+def dfdy (x, y) :
+    return 1/2 * (x - 2*y) * f(x, y)
+
+def dgdx (x, y) :
+    return 2*x
+
+def dgdy (x, y) :
+    return 6*y+6
 
 
 def DL (xyλ) :
