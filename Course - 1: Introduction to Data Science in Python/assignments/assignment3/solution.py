d34fce5e4ec3018a3a050791a9dff560e4b66c98
Ilyos Nishanov <80672859+ilyos-nishanov@users.noreply.github.com>
2023-05-01 11:31:09 +0000

    Mon, May 1, 2023, 4:31 PM +05:00

--- Course - 1: Introduction to Data Science in Python/assignments/assignment3/solution.py
+++ Course - 1: Introduction to Data Science in Python/assignments/assignment3/solution.py
@@ -2,3 +2,13 @@ import pandas as pd
 import numpy as np
 
 Energy = pd.read_excel('assets/Energy Indicators.xls', skiprows=17, skipfooter=38, na_values='...', usecols=[2,3,4,5], names=['Country', 'Energy Supply', 'Energy Supply per Capita', '% Renewable'])
+Energy['Energy Supply'] = Energy['Energy Supply'] * 1000000
+Energy['Country'] = Energy['Country'].str.replace(r" \(.*\)","")
+Energy['Country'] = Energy['Country'].str.replace(r"\d*","")
+
+Energy['Country'] = Energy['Country'].replace({"Republic of Korea": "South Korea",
+                                                "United States of America": "United States",
+                                                "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
+                                                "China, Hong Kong Special Administrative Region": "Hong Kong"
+                                                "Iran (Islamic Republic of)": "Iran"})
+Energy.set_index('Country').loc
