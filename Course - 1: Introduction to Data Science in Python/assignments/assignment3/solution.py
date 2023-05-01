554134b369c4e665ae521f252e06090b8ec31521
Ilyos Nishanov <80672859+ilyos-nishanov@users.noreply.github.com>
2023-05-01 11:35:09 +0000

    Mon, May 1, 2023, 4:35 PM +05:00

--- Course - 1: Introduction to Data Science in Python/assignments/assignment3/solution.py
+++ Course - 1: Introduction to Data Science in Python/assignments/assignment3/solution.py
@@ -1,18 +1,23 @@
 import pandas as pd
 import numpy as np
 import re
+import warnings
+warnings.filterwarnings('ignore')
 
 Energy = pd.read_excel('assets/Energy Indicators.xls', skiprows=17, skipfooter=38, na_values='...', usecols=[2,3,4,5], names=['Country', 'Energy Supply', 'Energy Supply per Capita', '% Renewable'])
-Energy['Energy Supply'] = Energy['Energy Supply'] * 1000000
-Energy['Country'] = Energy['Country'].str.replace(r" \(.*\)","")
-Energy['Country'] = Energy['Country'].str.replace(r"\d*","")
+print (Energy.head())
 
-Energy['Country'] = Energy['Country'].replace({"Republic of Korea": "South Korea",
-                                                "United States of America": "United States",
-                                                "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
-                                                "China, Hong Kong Special Administrative Region": "Hong Kong"})
-Energy.set_index('Country').loc['United States']
-for i in Energy['Country'].replace({'United States of America' : 'United States',
-                                    'United Kingdom of Great Britain and Northern Ireland':'United Kingdom',
-                                    'China, Hong Kong Special Administrative Region':'Hong Kong'}):
-    print(i)
+
+# Energy['Energy Supply'] = Energy['Energy Supply'] * 1000000
+# Energy['Country'] = Energy['Country'].str.replace(r" \(.*\)","")
+# Energy['Country'] = Energy['Country'].str.replace(r"\d*","")
+
+# Energy['Country'] = Energy['Country'].replace({"Republic of Korea": "South Korea",
+#                                                 "United States of America": "United States",
+#                                                 "United Kingdom of Great Britain and Northern Ireland": "United Kingdom",
+#                                                 "China, Hong Kong Special Administrative Region": "Hong Kong"})
+# Energy.set_index('Country').loc['United States']
+# for i in Energy['Country'].replace({'United States of America' : 'United States',
+#                                     'United Kingdom of Great Britain and Northern Ireland':'United Kingdom',
+#                                     'China, Hong Kong Special Administrative Region':'Hong Kong'}):
+#     print(i)
