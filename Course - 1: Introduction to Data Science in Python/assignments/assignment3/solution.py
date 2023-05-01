689e9d071ea51250f649341b0d3bff09879cb320
Ilyos Nishanov <80672859+ilyos-nishanov@users.noreply.github.com>
2023-05-01 12:00:50 +0000

    Mon, May 1, 2023, 5:00 PM +05:00

--- Course - 1: Introduction to Data Science in Python/assignments/assignment3/solution.py
+++ Course - 1: Introduction to Data Science in Python/assignments/assignment3/solution.py
@@ -15,10 +15,11 @@ def answer_one():
     GDP['Country Name'] = GDP['Country Name'].replace({"Korea, Rep.": "South Korea","Iran, Islamic Rep.": "Iran","Hong Kong SAR, China": "Hong Kong"})
     ScimEn = pd.read_excel('assets/scimagojr-3.xlsx')
     df = pd.merge(ScimEn,Energy,how='inner',left_on='Country',right_on='Country')
+    df = df[df['Rank']<=15]
+
     df = pd.merge(df,GDP,how='inner',left_on='Country',right_on='Country Name')
     df = df.set_index('Country')
     df = df[['Rank', 'Documents', 'Citable documents', 'Citations', 'Self-citations', 'Citations per document', 'H index','Energy Supply', 'Energy Supply per Capita', '% Renewable', '2006', '2007','2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']]
     df = df.sort_values(by='Rank')
-    df = df[df['Rank']<=15]
     return df
 answer_one()
