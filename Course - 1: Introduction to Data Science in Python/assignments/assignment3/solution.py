ee477a41c3d0ad316672358c6931f3b0ed2beafe
Ilyos Nishanov <80672859+ilyos-nishanov@users.noreply.github.com>
2023-05-01 12:11:45 +0000

    Mon, May 1, 2023, 5:11 PM +05:00

--- Course - 1: Introduction to Data Science in Python/assignments/assignment3/solution.py
+++ Course - 1: Introduction to Data Science in Python/assignments/assignment3/solution.py
@@ -32,14 +32,13 @@ def answer_two():
     Energy['Country'] = Energy['Country'].replace({"Republic of Korea": "South Korea","United States of America": "United States","United Kingdom of Great Britain and Northern Ireland": "United Kingdom","China, Hong Kong Special Administrative Region": "Hong Kong"})
     GDP = pd.read_csv('assets/world_bank.csv',skiprows=4)
     GDP['Country Name'] = GDP['Country Name'].replace({"Korea, Rep.": "South Korea","Iran, Islamic Rep.": "Iran","Hong Kong SAR, China": "Hong Kong"})
+    GDP = GDP.rename(columns={'Country Name': 'Country'})
     ScimEn = pd.read_excel('assets/scimagojr-3.xlsx')
     df = pd.merge(ScimEn,Energy,how='inner',left_on='Country',right_on='Country')
-    df = df[df['Rank']<=15]
-
-    df = pd.merge(df,GDP,how='inner',left_on='Country',right_on='Country Name')
-    df = df.set_index('Country')
-    df = df[['Rank', 'Documents', 'Citable documents', 'Citations', 'Self-citations', 'Citations per document', 'H index','Energy Supply', 'Energy Supply per Capita', '% Renewable', '2006', '2007','2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']]
-    df = df.sort_values(by='Rank')
-    return len(df)
+    inner_join_1 = pd.merge(df,GDP,how='inner',left_on='Country',right_on='Country')
+    inner_join_2 = pd.merge(ScimEn,Energy,how='inner',left_on='Country',right_on='Country')
+    outer_join_1 = pd.merge(inner_join_1,inner_join_2,how='outer',left_on='Country',right_on='Country')
+    outer_join_2 = pd.merge(inner_join_2,inner_join_1,how='outer',left_on='Country',right_on='Country')
+    return len(outer_join_1)-len(outer_join_2)
 answer_two()
 
