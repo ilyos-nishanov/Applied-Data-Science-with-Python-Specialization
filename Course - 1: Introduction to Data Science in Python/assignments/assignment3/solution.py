e0b5a1664ea4a39d84032bfe59f9d344518e8fbe
Ilyos Nishanov <80672859+ilyos-nishanov@users.noreply.github.com>
2023-05-01 11:25:12 +0000

    Mon, May 1, 2023, 4:25 PM +05:00

--- Course - 1: Introduction to Data Science in Python/assignments/assignment3/solution.py
+++ Course - 1: Introduction to Data Science in Python/assignments/assignment3/solution.py
@@ -1,3 +1,4 @@
 import pandas as pd
 import numpy as np
 
+Energy = pd.read_excel('assets/Energy Indicators.xls', skiprows=17, skipfooter=38, na_values='...', usecols=[2,3,4,5], names=['Country', 'Energy Supply', 'Energy Supply per Capita', '% Renewable'])
